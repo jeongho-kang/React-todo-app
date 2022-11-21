3ec0118025d1eb59a5f926c6019ca6dbdde8caa7
jeongho-kang <aj9037@naver.com>
2022-11-21 22:27:54 +0900

    11.21

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
 
 const App = () => {
-  return <div>To-do app을 만들자</div>
-}
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
+};
 
 export default App;
